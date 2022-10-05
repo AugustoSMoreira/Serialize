a72370f3428e547aa7b1345c4176663fc1651133
Augusto Moreira <gustoux@gmail.com>
2022-10-05 08:21:00 -0300

    Adicionar arquivos de projeto.

---
+++ Client.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Serialize
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string EnderecoCompleto { get; set; }
+
+        private static readonly string fileName = "Client.json";
+
+        public void Save()
+        {
+            var clients = Client.GetClients();
+            var clientsExist = clients.Find(c => c.Id == this.Id);
+            if (clientsExist != null)
+            {
+                Console.WriteLine("ARQUIVO EXISTE!");
+                clients.Remove(clientsExist);
+            }
+
+            clients.Add(this);
+
+            string clientsString = JsonSerializer.Serialize(clients);
+
+            File.WriteAllText(fileName, clientsString);
+        }
+
+        public static List<Client> GetClients()
+        {
+            _ = new List<Client>();
+            var clientsJson = File.ReadAllText(fileName);
+            List<Client>? clients = JsonSerializer.Deserialize<List<Client>>(clientsJson);
+            return clients;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Serialize;
+using System.Text.Json;
+
+new Client
+{
+    Id = 1,
+    Nome = "Augusto Moreira",
+    Email = "augusto@email.com",
+    Telefone = "2196969696",
+    EnderecoCompleto = "Rua Ali Perto"
+}.Save();
+
+/*new Client
+{
+    Id = 2,
+    Nome = "Enzo Moreira",
+    Email = "augusto@email.com",
+    Telefone = "2196969696",
+    EnderecoCompleto = "Rua Ali Perto"
+}.Save();*/
+
+/*new Client
+{
+    Id = 3,
+    Nome = "Jeniffer Moreira",
+    Email = "augusto@email.com",
+    Telefone = "2196969696",
+    EnderecoCompleto = "Rua Ali Perto"
+}.Save();*/
